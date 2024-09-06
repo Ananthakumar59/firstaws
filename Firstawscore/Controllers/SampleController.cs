792879c27e236bc540a957c34ad2b0f965c5d219
Ananthakumar59 <32794004+Ananthakumar59@users.noreply.github.com>
2024-09-06 12:01:00 +0530

    Initial commit

---
+++ Firstawscore/Controllers/SampleController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Firstawscore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SampleController : ControllerBase
+    {
+
+
+
+        public string Message()
+        {
+
+            return "Welcome to Aws";
+        }
+    }
+}
